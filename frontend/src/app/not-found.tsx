7d17d5da4f243bcfb3724b60434e273e725ccb6f
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-02-07 20:49:24 +0100

    feat(not-found): add 404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <main className="flex-1 flex flex-col justify-center items-center">
+      <h1 className="text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue-500 to-blue-700 hover:brightness-90">
+        404
+      </h1>
+      <p className="text-2xl font-semibold">Page not found</p>
+    </main>
+  )
+}
+
+export default Page
