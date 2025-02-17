1aa668fc73946feabc312a6283b7130b173fc49a
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-02-17 22:31:35 +0100

    feat(metas): change meta data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
       <div className="mt-10 flex gap-5">
         <Link
           href="/signup"
-          className="rounded bg-slate-700 px-4 py-2 text-white hover:bg-slate-600"
+          className="rounded bg-gradient-to-b from-blue-500 to-blue-700 px-4 py-2 text-white hover:bg-slate-600"
         >
           Sign Up
         </Link>
         <Link
           href="/signin"
-          className="rounded bg-slate-700 px-4 py-2 text-white hover:bg-slate-600"
+          className="rounded bg-gradient-to-b from-blue-500 to-blue-700 px-4 py-2 text-white hover:bg-slate-600"
         >
           Sign In
         </Link>
